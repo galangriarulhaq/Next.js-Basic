627820e58fce8d58acf7bbfb24a0ef0f9fd64c35
galangriarulhaq <galangrz337@gmail.com>
2023-10-31 23:26:54 +0700

    Login-multi-role

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,24 +1,10 @@
-import Link from "next/link";
-import styles from "./Login.module.scss";
-import { useRouter } from "next/router";
+import LoginView from "@/views/Auth/Login";
 
 const LoginPage = () => {
-  const { push } = useRouter();
-
-  const handlerLogin = () => {
-    push("/");
-  };
-
   return (
-    <div className={styles.login}>
-      <h1>Login Pages</h1>
-      <br />
-      <button onClick={handlerLogin}>Login</button>
-      <br />
-      <p>
-        Apakah sudah punya akun ? registrasi <Link href={"/auth/register"}>disini</Link>
-      </p>
-    </div>
+    <>
+      <LoginView />
+    </>
   );
 };
 
---
+++ src/views/Auth/Login/index.tsx
@@ -0,0 +1,69 @@
+import Link from "next/link";
+import styles from "./Login.module.scss";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { signIn } from "next-auth/react";
+
+const LoginView = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const { push, query } = useRouter();
+
+  const callbackUrl: any = query.callbackUrl || "/";
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    setError("");
+    setIsLoading(true);
+
+    try {
+      const res: any = await signIn("credentials", {
+        redirect: false,
+        email: event.target.email.value,
+        password: event.target.password.value,
+        callbackUrl,
+      });
+
+      if (!res.error) {
+        setIsLoading(false);
+        push(callbackUrl);
+      } else {
+        setIsLoading(false);
+        setError("Email or password is incorrect");
+      }
+    } catch (error: any) {
+      setIsLoading(false);
+      setError("Email or password is incorrect");
+    }
+  };
+  return (
+    <div className={styles.login}>
+      <h1 className={styles.login__title}>Login</h1>
+      {error && <p className={styles.login__error}>{error}</p>}
+      <div className={styles.login__form}>
+        <form onSubmit={handleSubmit}>
+          <div className={styles.login__form__item}>
+            <label htmlFor="email" className={styles.login__form__item__label}>
+              Email
+            </label>
+            <input type="email" name="email" id="email" placeholder="email" className={styles.login__form__item__input} />
+          </div>
+          <div className={styles.login__form__item}>
+            <label htmlFor="password" className={styles.login__form__item__label}>
+              Password
+            </label>
+            <input type="password" name="password" id="password" placeholder="password" className={styles.login__form__item__input} />
+          </div>
+          <button type="submit" className={styles.login__form__item__button} disabled={isLoading}>
+            {isLoading ? "Loading..." : "Login"}
+          </button>
+        </form>
+      </div>
+      <p className={styles.login__link}>
+        Don't have an account? Sign up <Link href="/auth/register">here</Link>
+      </p>
+    </div>
+  );
+};
+
+export default LoginView;
